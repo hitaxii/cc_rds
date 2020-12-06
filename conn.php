91fc16c81aeec8c1ea2308eb7cc0c8a3fccaf9ac
hitaxii <51741754+hitaxii@users.noreply.github.com>
2020-12-06 08:54:52 -0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,3 +1,3 @@
 <?php 
-	$con = mysqli_connect('localhost','root','','studentdb');
+	$con = mysqli_connect('studentdb.cmivnl0pjak7.us-east-1.rds.amazonaws.com','admin','hitaxii055','studentdb');
  ?>
